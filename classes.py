be34db02a4c0e99cfe52cb8b316b63ecc65b4ad9
EVELLYN THAUANY ALVES DE LIMA <evellyn.alves@academico.ifrn.edu.br>
2024-07-17 12:17:15 -0300

    classes

---
+++ classes.py
@@ -0,0 +1,23 @@
+class Veiculo:
+    def _init_(self, marca, modelo):
+        self.marca = marca             # Atributo público
+        self._ano = 2020               # Atributo protegido
+        self.__quilometragem = 0       # Atributo privado
+
+    def dirigir(self, distancia):
+        self.__quilometragem += distancia
+
+    def get_quilometragem(self):
+        return self.__quilometragem
+
+
+class Carro(Veiculo):
+    def _init_(self, marca, modelo, tipo_combustivel):
+        super()._init_(marca, modelo)
+        self.tipo_combustivel = tipo_combustivel  # Atributo público
+
+    def abastecer(self):
+        print(f"Abastecendo o carro {self.marca} {self.modelo}.")
+
+    def get_ano(self):
+        return self._ano  # Acesso ao atributo protegido
---
+++ teste.py
@@ -0,0 +1,12 @@
+from classes import Carro
+
+def teste_veiculos():
+    carro = Carro("Toyota", "Corolla", "Gasolina")
+    carro.dirigir(150)
+    print(f"Marca: {carro.marca}, Modelo: {carro.modelo}, Tipo: {carro.tipo_combustivel}")
+    print(f"Quilometragem: {carro.get_quilometragem()} km")
+    print(f"Ano do veículo: {carro.get_ano()}")
+    carro.abastecer()
+
+if __name__ == "_main_":
+    teste_veiculos()
