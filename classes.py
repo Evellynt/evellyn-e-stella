13b2b8562d9f6dea29c95b86a784c70b293ec0ff
xstellas2 <m.stella@escolar.ifrn.edu.br>
2024-07-23 14:58:54 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,8 +1,8 @@
 class Veiculo:
     def _init_(self, marca, modelo):
-        self.marca = marca             # Atributo público
-        self._ano = 2020               # Atributo protegido
-        self.__quilometragem = 0       # Atributo privado
+        self.marca = marca             
+        self._ano = 2020               
+        self.__quilometragem = 0       
 
     def dirigir(self, distancia):
         self.__quilometragem += distancia
@@ -14,11 +14,11 @@ class Veiculo:
 class Carro(Veiculo):
     def _init_(self, marca, modelo, tipo_combustivel):
         super()._init_(marca, modelo)
-        self.tipo_combustivel = tipo_combustivel  # Atributo público
+        self.tipo_combustivel = tipo_combustivel  
 
     def abastecer(self):
         print(f"Abastecendo o carro {self.marca} {self.modelo}.")
 
     def get_ano(self):
-        return self._ano  # Acesso ao atributo protegido
+        return self._ano  
     
